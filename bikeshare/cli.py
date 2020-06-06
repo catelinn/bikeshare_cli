5da71051ad1768f2fd533ae3f59a866cc7c51c1b
Catelinn <catelinn.x@gmail.com>
2020-06-06 01:39:26 -0700

    Add subcommand to bikeshare

--- bikeshare/cli.py
+++ bikeshare/cli.py
@@ -1,34 +1,59 @@
-import time
-
 from pkg_resources import resource_stream
+
 import click
 import numpy as np
 import pandas as pd
-from .helpfunc import check_col, validate_months, validate_days
 
+from .helpfunc import validate_days, validate_months, show_data, station_stats,\
+    trip_duration_stats, time_stats, user_stats, restart_program
 
 CITY_DATA = { 'Chicago': 'data/chicago.csv',
               'New York': 'data/new_york_city.csv',
               'Washington': 'data/washington.csv' }
 
-def load_data(city, month, day):
-    """
-    Loads data for the specified city and filters by month and day if applicable.
-    pd.read_csv(city)
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
-    Args:
-        (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
-    Returns:
-        df - Pandas DataFrame containing city data filtered by month and day
+class Config:
+    def __init__(self):
+        self.city=''
+
+
+pass_config = click.make_pass_decorator(Config, ensure=True)
+
+@click.group(context_settings=CONTEXT_SETTINGS)
+@click.argument('city', type=click.Choice(['chicago', 'new york', 'washington']))
+@pass_config
+def main(config, city):
     """
-    stream = resource_stream(__name__, CITY_DATA[city.title()])
+    Usage: 
+    bikeshare city filter -m 23 -d 67 --s summary -time -station -trip -user
+
+    This script explore bikeshare data for a specific city.
+    """
+    click.clear()
+    click.echo(f'Hello! Let\'s explore some US bikeshare data for {city}!\n')
+    config.city = city
+    
+    
+@main.command('filter')
+@click.option('-month', '-m', type=click.INT,callback=validate_months, prompt=True,
+             help="Enter 0 for all months, 1 for January and so on, maximum is 6 (up to June).")
+@click.option('-day', '-d', type=click.INT, callback=validate_days, prompt=True,
+             help="Enter 0 for all days of a week, 1 for Monday and so on, maximum is 7 (up to Sunday).")
+@click.option('--show', '--s', default=True, is_flag=True,
+              help='Show data table, 5 lines each time, if flagged')
+@click.option('--line', '--l', type=click.INT, default=5, help="Number of lines of data to show")
+@pass_config
+def filter(config, month, day, show, line):
+    """
+    Filter city data by month and day.
+    """
+    stream = resource_stream(__name__, CITY_DATA[config.city.title()])
     df = pd.read_csv(stream)
 
     # all column names having the first letter of each work capitalized 
     df.rename(str.title, axis=1)
-    
+
     # convert the Start Time column to datetime
     df['Start Time'] = pd.to_datetime(df['Start Time'])
 
@@ -36,165 +61,34 @@ def load_data(city, month, day):
     df['month'] = df['Start Time'].dt.month
     df['day_of_week'] = df['Start Time'].dt.day_name()
 
+    df_copy = df.copy()
 
-    # filter by month(s) if applicable
+    # filter by month(s) 
     if 0 not in month:
         # filter by month(s) to create the new dataframe
-        df = df[df['month'].isin(month)]
+        df_copy = df_copy[df_copy['month'].isin(month)]
 
-    # filter by day of week if applicable
+    # filter by day of week
     if 0 not in day:
         days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
         filters = [days[i-1] for i in day]
         # filter by day of week to create the new dataframe
-        df = df[df['day_of_week'].str.lower().isin(filters)]
+        df_copy = df_copy[df_copy['day_of_week'].str.lower().isin(filters)]
     
-    return df
+    # Show dataframe information
+    click.echo(f"{df_copy.info()}\n")
+
+    # Show data (default True and Line number as 10)
+    if show:
+        show_data(df_copy, line)
 
 
+    # Show stats
+    time_stats(df_copy)
+    station_stats(df_copy)
+    trip_duration_stats(df_copy)
+    user_stats(df_copy)
 
-def show_data(df):
-    """
-    After filtering the data, prompt user if to display 5 lines of raw data each time,
-    show the data if the answer is 'yes'; continue the prompt until the user says no.
-
-    Args:
-        (DataFrame) df - the filtered dataset available for user to view and explore
-    Return:
-        Null
-    """
-    # reset df index to start from 0
-    df = df.reset_index()
-
-    # use generator to get indices which are multiple of 5 and 
-    # stops at the total number of df rows
-    index_gen = (x for x in range(0, df.shape[0], 5))
-
-    while True:
-        try:
-            i = next(index_gen)
-            if i >=5:
-                if click.confirm('Do you want to continue viewing data?') is False:
-                    break
-            click.echo(df[i:i+5])
-        except StopIteration:
-            break
-
+    if click.confirm(f"Restart to select different month(s) or day(s) for {config.city.title()}?") is not False:
+        restart_program()
         
-def time_stats(df):
-    """Displays statistics on the most frequent times of travel."""
-
-    click.echo('\nCalculating The Most Frequent Times of Travel...\n')
-    start_time = time.time()
-
-    if check_col(df, 'Start Time'):
-        # TO DO: display the most common month
-        click.echo(f"The most common month is:{df['month'].mode()[0]}")
-
-        # TO DO: display the most common day of week
-        click.echo(f"The most common day of week is:{df['day_of_week'].mode()[0]}")
-
-        # TO DO: display the most common start hour
-        click.echo(f"The most common start hour is:{df['Start Time'].dt.hour.mode()[0]}")
-
-    click.echo(f"\nThis took {time.time() - start_time} seconds")
-    click.echo('-'*40)
-
-
-def station_stats(df):
-    """Displays statistics on the most popular stations and trip."""
-
-    click.echo('\nCalculating The Most Popular Stations and Trip...\n')
-    start_time = time.time()
-
-    if check_col(df, 'Start Station'):
-        # TO DO: display most commonly used start station
-        click.echo(f"The most commonly used start station:{df['Start Station'].mode()[0]}")
-
-    if check_col(df, 'End Station'):
-        # TO DO: display most commonly used end station
-        click.echo(f"The most commonly used end station:{df['End Station'].mode()[0]}")
-
-    # TO DO: display most frequent combination of start station and end station trip
-    if check_col(df, 'Start Station') and check_col(df, 'End Station') :
-        click.echo("The most frequent combination of start station and end station trip:{}".format((df['Start Station']+' | '+df['End Station']).mode()[0]))
-
-    click.echo(f"\nThis took {time.time() - start_time} seconds")
-    click.echo('-'*40)
-
-
-def trip_duration_stats(df):
-    """Displays statistics on the total and average trip duration."""
-
-    click.echo('\nCalculating Trip Duration...\n')
-    start_time = time.time()
-
-    if check_col(df, 'Trip Duration'):
-        # TO DO: display total travel time
-        click.echo(f"Total travel time:{df['Trip Duration'].sum()}" )
-
-        # TO DO: display mean travel time
-        click.echo(f"The average travel time: {df['Trip Duration'].mean()}")
-
-    click.echo(f"\nThis took {time.time() - start_time} seconds")
-    click.echo('-'*40)
-
-
-def user_stats(df):
-    """Displays statistics on bikeshare users."""
-
-    click.echo('\nCalculating User Stats...\n')
-    start_time = time.time()
-
-    # TO DO: Display counts of user types
-    if check_col(df, 'User Type'): 
-        click.echo(f"{pd.DataFrame(df['User Type'].value_counts())}\n")
-
-    # TO DO: Display counts of gender
-    if check_col(df, 'Gender'):
-        click.echo(f"{pd.DataFrame(df['Gender'].value_counts())}\n")
-
-    # TO DO: Display earliest, most recent, and most common year of birth
-    if check_col(df, 'Birth Year'):
-        click.echo(f"The birth year of the oldest user is: {df['Birth Year'].min()}")
-        click.echo(f"The birth year of the youngest user is: {df['Birth Year'].max()}")
-        click.echo(f"The most common birth year of users is: {df['Birth Year'].mode()[0]}")
-    
-
-    click.echo(f"\nThis took {time.time() - start_time} seconds")
-    click.echo('-'*40)
-
-
-@click.command()
-@click.argument('city', type=click.Choice(['chicago', 'new york', 'washington']))
-@click.option('-month', '-m', type=click.INT,callback=validate_months, prompt=True,
-             help="Enter 0 for all months, 1 for January and so on, maximum is 6 (up to June).")
-@click.option('-day', '-d', type=click.INT, callback=validate_days, prompt=True,
-             help="Enter 0 for all days of a week, 1 for Monday and so on, maximum is 7 (up to Sunday).")
-@click.option('--show/--no-show', '--s/--no-s', default=True,
-              help='Show data table, 5 lines each time, if flagged')
-def main(city, month, day, show):
-    """
-    This script explore bikeshare data for a specific city.
-
-    Usage: bikeshare [-md][--s] city
-    """
-    click.clear()
-    click.echo('Hello! Let\'s explore some US bikeshare data!\n')
-
-    while True:
-        df = load_data(city, month, day)
-        click.echo(f"{df.info()}\n")
-        click.pause()
-
-        if show:
-            show_data(df)
-
-        time_stats(df)
-        station_stats(df)
-        trip_duration_stats(df)
-        user_stats(df)
-
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
-        if restart.lower() != 'yes':
-            break
--- bikeshare/helpfunc.py
+++ bikeshare/helpfunc.py
@@ -1,6 +1,18 @@
-import pandas as pd
-import numpy as np
+import time
 import click
+import numpy as np
+import pandas as pd
+
+import sys
+import os
+
+
+def restart_program():
+    """Restarts the current program.
+    Note: this function does not return. Any cleanup action (like
+    saving data) must be done before calling this function."""
+    python = sys.executable
+    os.execl(python, python, * sys.argv)
 
 
 def validate_intval(ctx, value, valid, errmsg):
@@ -16,7 +28,6 @@ def validate_intval(ctx, value, valid, errmsg):
     finally:
         if len(result)==0:
             raise click.BadParameter(errmsg)
-        click.pause() # wait for user press key to continue here
         return result
 
 def validate_months(ctx, value):
@@ -25,9 +36,33 @@ def validate_months(ctx, value):
     return validate_intval(ctx, value, valid, errmsg)
 
 def validate_days(ctx, value):
+    # 012 only returns [1,2] only
     valid = [0,1,2,3,4,5,6,7]
     errmsg = 'day can only be from 0 to 7 (0 as for all days available)'
     return validate_intval(ctx, value, valid, errmsg)
+
+
+def show_data(df, line_num):
+    """
+    After filtering the data, prompt user if to display 5 lines of raw data each time,
+    show the data if the answer is 'yes'; continue the prompt until the user says no.
+    """
+    # reset df index to start from 0
+    df = df.reset_index()
+
+    # use generator to get indices which are multiple of 5 and 
+    # stops at the total number of df rows
+    index_gen = (x for x in range(0, df.shape[0], line_num))
+
+    while True:
+        try:
+            i = next(index_gen)
+            if i >=line_num:
+                if click.confirm('Do you want to continue viewing data?') is False:
+                    break
+            click.echo(df[i:i+line_num])
+        except StopIteration:
+            break
 
 
 def check_col(df, col_name):
@@ -36,3 +71,88 @@ def check_col(df, col_name):
     else:
         click.echo("No data on {} for this city. ".format(col_name))
         return False
+
+
+def time_stats(df):
+    """Displays statistics on the most frequent times of travel."""
+
+    click.echo('\nCalculating The Most Frequent Times of Travel...\n')
+    start_time = time.time()
+
+    if check_col(df, 'Start Time'):
+        # TO DO: display the most common month
+        click.echo(f"The most common month is:{df['month'].mode()[0]}")
+
+        # TO DO: display the most common day of week
+        click.echo(f"The most common day of week is:{df['day_of_week'].mode()[0]}")
+
+        # TO DO: display the most common start hour
+        click.echo(f"The most common start hour is:{df['Start Time'].dt.hour.mode()[0]}")
+
+    click.echo(f"\nThis took {time.time() - start_time} seconds")
+    click.echo('-'*40)
+
+
+def station_stats(df):
+    """Displays statistics on the most popular stations and trip."""
+
+    click.echo('\nCalculating The Most Popular Stations and Trip...\n')
+    start_time = time.time()
+
+    if check_col(df, 'Start Station'):
+        # TO DO: display most commonly used start station
+        click.echo(f"The most commonly used start station:{df['Start Station'].mode()[0]}")
+
+    if check_col(df, 'End Station'):
+        # TO DO: display most commonly used end station
+        click.echo(f"The most commonly used end station:{df['End Station'].mode()[0]}")
+
+    # TO DO: display most frequent combination of start station and end station trip
+    if check_col(df, 'Start Station') and check_col(df, 'End Station') :
+        click.echo("The most frequent combination of start station and end station trip:{}".format((df['Start Station']+' | '+df['End Station']).mode()[0]))
+
+    click.echo(f"\nThis took {time.time() - start_time} seconds")
+    click.echo('-'*40)
+
+
+
+def trip_duration_stats(df):
+    """Displays statistics on the total and average trip duration."""
+
+    click.echo('\nCalculating Trip Duration...\n')
+    start_time = time.time()
+
+    if check_col(df, 'Trip Duration'):
+        # TO DO: display total travel time
+        click.echo(f"Total travel time:{df['Trip Duration'].sum()}" )
+
+        # TO DO: display mean travel time
+        click.echo(f"The average travel time: {df['Trip Duration'].mean()}")
+
+    click.echo(f"\nThis took {time.time() - start_time} seconds")
+    click.echo('-'*40)
+
+
+def user_stats(df):
+    """Displays statistics on bikeshare users."""
+
+    click.echo('\nCalculating User Stats...\n')
+    start_time = time.time()
+
+    # TO DO: Display counts of user types
+    if check_col(df, 'User Type'): 
+        click.echo(f"{pd.DataFrame(df['User Type'].value_counts())}\n")
+
+    # TO DO: Display counts of gender
+    if check_col(df, 'Gender'):
+        click.echo(f"{pd.DataFrame(df['Gender'].value_counts())}\n")
+
+    # TO DO: Display earliest, most recent, and most common year of birth
+    if check_col(df, 'Birth Year'):
+        click.echo(f"The birth year of the oldest user is: {df['Birth Year'].min()}")
+        click.echo(f"The birth year of the youngest user is: {df['Birth Year'].max()}")
+        click.echo(f"The most common birth year of users is: {df['Birth Year'].mode()[0]}")
+    
+
+    click.echo(f"\nThis took {time.time() - start_time} seconds")
+    click.echo('-'*40)
